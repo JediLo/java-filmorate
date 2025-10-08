8acaa2a5ef8a0efbd20c90ddba1401fe1a9b32cd
JediLon <r.lipatenkov@gmail.com>
2025-10-08 04:58:57 +0300

    Fix CodeStyle

--- src/test/java/ru/yandex/practicum/filmorate/storage/user/UserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/UserStorageTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
